5e9f8982f4cd394f70802cac5d3b97c5e64e04ab
mujtaba7vals <39899184+mujtaba7vals@users.noreply.github.com>
2018-06-21 16:46:02 +0500

    Add files via upload

--- calendar.rb
+++ calendar.rb
@@ -2,8 +2,8 @@
 require 'date'
 require_relative 'event_details'
 class Calendar
-  @@events_details = Hash.new { |hash, key| hash[key] = [] }
-  @@id = 0
+  @events_details = Hash.new { |hash, key| hash[key] = [] }
+  @id = 0
 
   def self.add_event
     check = 'y'
@@ -13,8 +13,8 @@ class Calendar
       puts 'Enter date for the event (yyyy/mm/dd)'
       date = gets.strip
       date = Date.parse(date)
-      @@id += 1
-      @@events_details[date] << EventDetails.new(@@id, name, date)
+      @id += 1
+      @events_details[date] << EventDetails.new(@id, name, date)
       puts 'Event added'
       puts 'Do you wish to add another event(y/n)?'
       check = gets.strip.downcase
@@ -24,8 +24,8 @@ class Calendar
   end
 
   def self.return_all_events
-    if !@@events_details.empty?
-      @@events_details.each do |key, event_detail|
+    if !@events_details.empty?
+      @events_details.each do |key, event_detail|
         event_detail.each do |event|
           puts "ID: #{event.id} Name: #{event.event_name.ljust(7)} Date: #{key.strftime('%Y/%m/%d')}"
         end
@@ -36,7 +36,7 @@ class Calendar
   end
 
   def self.events_for_given_month
-    if !@@events_details.empty?
+    if !@events_details.empty?
       puts 'Enter the required year and month (yyyy/mm)'
       date = Date.parse(gets.strip)
       firstday = date.wday
@@ -47,8 +47,8 @@ class Calendar
       dates.each_slice(7) do |week|
         puts week.map { |day|
                key = Date.new(date.year, date.month, day) unless day.nil?
-               if @@events_details.key? key
-                 month_event_detail_hash[key] = @@events_details[key]
+               if @events_details.key? key
+                 month_event_detail_hash[key] = @events_details[key]
                  "[#{'%02i' % day}]"
                elsif day.nil?
                  day.to_s.rjust(3).ljust(4)
@@ -72,11 +72,11 @@ class Calendar
   end
 
   def self.events_for_given_date
-    if !@@events_details.empty?
+    if !@events_details.empty?
       puts 'Enter the required date (yyyy/mm/dd)'
       date = Date.parse(gets.strip)
-      if @@events_details.key? date
-        @@events_details[date].each do |event|
+      if @events_details.key? date
+        @events_details[date].each do |event|
           puts "ID: #{event.id} Name: #{event.event_name.ljust(7)} Date: #{event.date.strftime('%Y/%m/%d')}"
         end
       else
@@ -90,18 +90,18 @@ class Calendar
   end
 
   def self.delete_event
-    if !@@events_details.empty?
+    if !@events_details.empty?
       puts 'Enter an Id from the list of events given below'
       return_all_events
       flag = 0
       id_entered = gets.strip.to_i
-      @@events_details.each do |key, event_detail|
+      @events_details.each do |key, event_detail|
         event_detail.each do |event|
           if event.id == id_entered
-            @@events_details[key].delete(event)
-            @@events_details.delete(key) if @@events_details[key].empty?
+            @events_details[key].delete(event)
+            @events_details.delete(key) if @events_details[key].empty?
             puts 'Event Deleted'
-            @@id = 0 if @@events_details.empty?
+            @id = 0 if @events_details.empty?
             flag = 1
             break
           end
